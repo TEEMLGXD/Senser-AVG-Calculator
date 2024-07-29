e9546df9785f713ded63a2b4c478b45b9d4804a8
TEEMLGXD <aunghuab@gmail.com>
2024-07-29 11:30:33 +0700

    Update Check_and_Calculate.ino

--- POP32_Senser_Calculator/Check_and_Calculate.ino
+++ POP32_Senser_Calculator/Check_and_Calculate.ino
@@ -13,17 +13,18 @@ void check()
   waitSW_A(); //รอจนกว่าจะกดปุ่ม A เพื่อรับค่าสีขาวเข้ามา
   int white[8] = {ag(0),ag(1),ag(2),ag(3),ag(4),ag(5),ag(6),ag(7)};
   oled.clear();
-  for(int shw = 0; shw <= 7; shw++) //แสดงผลค่าที่ได้รับเข้ามาเพื่อตรวจสอบความถูกต้อง
+  for(int shw = 0; shw < 8; shw++) //แสดงผลค่าที่ได้รับเข้ามาเพื่อตรวจสอบความถูกต้อง
   {
     oled.text(shw,0,"%d",white[shw]);
   }
   oled.show();
   waitSW_B(); //รอให้กด B เพื่อล้าง oled
   oled.clear();
-  
-  delay(200);
+
+  delay(400);
   oled.text(0,0,"A to get black value");
   oled.show();
+  waitSW_A();
   int black[8] = {ag(0),ag(1),ag(2),ag(3),ag(4),ag(5),ag(6),ag(7)};
   oled.clear();
   for(int shb = 0; shb <= 7; shb++) //แสดงผลค่าที่ได้รับเข้ามาเพื่อตรวจสอบความถูกต้อง
@@ -35,18 +36,18 @@ void check()
   oled.clear();
 
   int AVGValue[8]; //กำหนดตัวแปรเพื่อประมวลผลหาค่ากลาง
-  for(int i; i <= 7; i++) //คำนวณค่ากลาง
+  for(int i = 0; i <= 7; i++) //คำนวณค่ากลาง
   {
     AVGValue[i] = (white[i] + black[i]) /2;
   }
-  
-  for(int ii; ii <= 7; ii++) //แสดงผลค่ากลาง
+
+  for(int ii = 0; ii <= 7; ii++) //แสดงผลค่ากลาง
   {
     oled.text(ii,0,"%d",AVGValue[ii]);
   }
-
   oled.show();
 }
+
   void setup() 
 {
   check();
